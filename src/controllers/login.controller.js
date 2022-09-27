00044bc206ac61d200406ccfc27908b932e250a0
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-09-27 16:04:33 -0300

    acrescenta instancia de error no service de login

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -3,11 +3,17 @@ const LoginService = require('../services/login.service');
 const loginHandler = (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).json({ message: 'Some required fields are missing' });
+    const err = new Error('Some required fields are missing');
+    err.status = 400;
+    throw err;
   }
 
   const user = LoginService.loginHandler(email, password);
-  if (!user) return res.status(400).json({ message: 'Invalid fields' });
+  if (!user) {
+    const err = new Error('Invalid fields');
+    err.status = 400;
+    throw err;
+  }
 
   res.status(200).json({ token: '' });
 };
