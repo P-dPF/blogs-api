9e5ed2fb241b8c60fe41da6ac9d91bddd0e1d3d4
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-09-27 17:01:21 -0300

    conserta erro de primary key sequelize

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,6 @@
 const UserModel = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-    },
+    id: DataTypes.INTEGER,
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
