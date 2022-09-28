f8ee9ff65a0c88131d3e524271b8bc6a05f125c6
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-09-28 20:19:55 -0300

    inclui validação em GET /post

--- src/routers/post.routes.js
+++ src/routers/post.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const { PostController } = require('../controllers');
+const { validateToken } = require('../middlewares');
 
 const router = express.Router();
 
+router.use(validateToken);
 router.get('/', PostController.getAllPosts);
 
 module.exports = router;
